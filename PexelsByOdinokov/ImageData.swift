af338d3e5848937747ce30ab5121010998e5718f
gralod95 <gralod95@mail.ru>
2024-06-26 12:04:38 +0200

    Add files via upload

---
+++ PexelsByOdinokov/ImageData.swift
@@ -0,0 +1,18 @@
+//
+//  ImageData.swift
+//  PexelsByOdinokov
+//
+//  Created by Odinokov G. A. on 17.06.2024.
+//
+
+import UIKit
+
+struct ImageData: ImageShortInfo {
+    let id: Int
+    let imageName: String
+    let ownerName: String
+    let previewUrl: String
+    let imageUrl: String
+    var previewImage: ImageState = .notLoaded
+    var image: ImageState = .notLoaded
+}
